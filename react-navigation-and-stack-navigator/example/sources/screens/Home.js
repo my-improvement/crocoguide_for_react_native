5b7d7d490b94a7b91ce59c920ea00265741a79f8
Naufal Hanif <nahanifg@gmail.com>
2020-10-20 15:28:53 +0700

    Update Home.js

--- react-navigation-and-stack-navigator/example/sources/screens/Home.js
+++ react-navigation-and-stack-navigator/example/sources/screens/Home.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
 export default class Home extends Component {
-  return (
-    <View style = {{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>
-        Home
-      </Text>
-    </View>
-  )
+  render() {
+    return (
+      <View style = {{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>
+          Home
+        </Text>
+      </View>
+    )
+  }
 }
