b130d0cdfc44ef036258975a416e5082d0718292
Naufal Hanif <nahanifg@gmail.com>
2020-10-20 14:29:07 +0700

    Update App.js

---
+++ react-navigation-and-stack-navigator/example/App.js
@@ -0,0 +1,28 @@
+>####Example
+
+```
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './sources/screens/Home.js'
+import Splashscreen from './sources/screens/Splashscreen.js'
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator
+          screenOptions = {{
+            headerShown: false
+          }}
+        >
+            <Stack.Screen name="Splashscreen" component={Splashscreen} />
+            <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+```
+  
