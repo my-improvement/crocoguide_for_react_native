87ce6a36aefec3ee81cae6efd2fda66393115eb1
Naufal Hanif <nahanifg@gmail.com>
2020-10-20 14:37:34 +0700

    Update Home.js

--- react-navigation-and-stack-navigator/example/sources/screens/Home.js
+++ react-navigation-and-stack-navigator/example/sources/screens/Home.js
@@ -2,14 +2,11 @@
 import React, { Component } as React from 'react';
 import { View, Text } from 'react-native'
 
-export default class App extends Component {
-  componentDidMount() {
-    setTimeout(() => this.props.navigation.replace("Home"), 1500)        
-  }
+export default class Home extends Component {
   return (
     <View style = {{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>
-        Splashscreen
+        Home
       </Text>
     </View>
   );
