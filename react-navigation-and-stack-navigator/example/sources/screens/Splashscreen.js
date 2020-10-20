3d1817f1388fbafbd0a6baa08aa6e04813333874
Naufal Hanif <nahanifg@gmail.com>
2020-10-20 14:38:40 +0700

    Update Splashscreen.js

---
+++ react-navigation-and-stack-navigator/example/sources/screens/Splashscreen.js
@@ -0,0 +1,16 @@
+```
+import React, { Component } as React from 'react';
+import { View, Text } from 'react-native'
+
+export default class Splashscreen extends Component {
+  componentDidMount() {
+    setTimeout(() => this.props.navigation.replace("Home"), 1500)        
+  }
+  return (
+    <View style = {{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>
+        Splashscreen
+      </Text>
+    </View>
+  );
+```
