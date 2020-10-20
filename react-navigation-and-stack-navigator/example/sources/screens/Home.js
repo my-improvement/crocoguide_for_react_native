19c8778a911de331b6a2d920cd471a2d884b1706
Naufal Hanif <nahanifg@gmail.com>
2020-10-20 15:02:25 +0700

    Update Home.js

--- react-navigation-and-stack-navigator/example/sources/screens/Home.js
+++ react-navigation-and-stack-navigator/example/sources/screens/Home.js
@@ -1,5 +1,5 @@
 ```
-import React, { Component } as React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native'
 
 export default class Home extends Component {
